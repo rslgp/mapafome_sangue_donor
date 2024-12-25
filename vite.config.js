45566524ad2d445b94da604109d08724b9b5f5ef
Rafael Pereira <rafael.pereira@medicalsys.com.br>
2024-12-25 00:04:18 -0300

    expose

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mapafome_sangue_donor/'
+  base: '/mapafome_sangue_donor/',
+  server: {
+    port: 8080
+  }
 })
